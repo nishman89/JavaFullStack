53c0c0ec93666a577c11c08bf5d50f476bf1a314
unknown <nish_mandal@hotmail.com>
2025-09-08 14:55:39 +0100

    End of REST API lesson

--- Northwind/src/main/java/com/sparta/northwind/repositories/CustomerRepository.java
+++ Northwind/src/main/java/com/sparta/northwind/repositories/CustomerRepository.java
@@ -2,6 +2,9 @@ package com.sparta.northwind.repositories;
 
 import com.sparta.northwind.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
+@RepositoryRestResource(exported = false)
 public interface CustomerRepository extends JpaRepository<Customer, String> {
 }
